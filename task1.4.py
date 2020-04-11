54bddf271bd2721312de93537e99d74aa2927882
Ирина <airinh1976@yandex.ru>
2020-04-11 21:25:20 +0300

    Task 1

---
+++ task1.4.py
@@ -0,0 +1,12 @@
+nat = input ('введите целое положительное число: ')
+while not nat.isdigit():
+    nat = input('Пожалуйста, введите число правильно: ')
+natwork = int(nat)
+nat1 =  natwork % 10
+nat2 = 0
+while natwork > 0 :
+    nat2 = (natwork // 10) % 10
+    if nat1 < nat2:
+        nat1 = nat2
+    natwork = int((natwork - nat2) / 10)
+print('наибольшая цифра в числе', nat, '-', nat1)
