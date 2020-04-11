f834ac14adeaab327dd4d2d442c816400b855612
Ирина <airinh1976@yandex.ru>
2020-04-11 20:29:08 +0300

    Task 1

--- task1.3.py
+++ task1.3.py
@@ -1,3 +1,7 @@
 n = input('Введите число: ')
 while not n.isdigit():
-    input('Пожалуйста, введите число правильно: ')
+    n = input('Пожалуйста, введите число правильно: ')
+n = str(n)
+nn = n * 2
+nnn = n * 3
+print(f'итог = {int(n) + int(nn) + int(nnn)}')
