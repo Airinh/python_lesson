f755c2045ed265af5264f43484978aa4f0ad0bc1
Ирина <airinh1976@yandex.ru>
2020-04-11 21:38:51 +0300

    Task 1.4

--- task1.4.py
+++ task1.4.py
@@ -2,9 +2,14 @@ nat = input ('введите целое положительное число: '
 while not nat.isdigit():
     nat = input('Пожалуйста, введите число правильно: ')
 natwork = int(nat)
-nat1 =  natwork % 10
+while natwork < 0:
+    nat = input('Пожалуйста, введите число правильно: ')
+while not nat.isdigit():
+    nat = input('Пожалуйста, введите число правильно: ')
+natwork = int(nat)
+nat1 = natwork % 10
 nat2 = 0
-while natwork > 0 :
+while natwork > 0:
     nat2 = (natwork // 10) % 10
     if nat1 < nat2:
         nat1 = nat2
