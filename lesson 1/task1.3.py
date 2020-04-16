067685ffa9a28ba943b40706c0ab4b78825b90ce
Airinh <63346957+Airinh@users.noreply.github.com>
2020-04-16 19:02:41 +0300

    Delete task1.3.py

--- lesson 1/task1.3.py
+++
@@ -1,7 +0,0 @@
-n = input('Введите число: ')
-while not n.isdigit():
-    n = input('Пожалуйста, введите число правильно: ')
-n = str(n)
-nn = n * 2
-nnn = n * 3
-print(f'итог = {int(n) + int(nn) + int(nnn)}')
